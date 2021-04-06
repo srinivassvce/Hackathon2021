44e431fa20b35be3d7cc16d7d9b773b962406758
Singh J <lohith.singh@siemens-healthineers.com>
2021-04-06 18:35:33 +0530

    Login,Register

--- smart-health/src/login/login.tsx
+++ smart-health/src/login/login.tsx
@@ -8,13 +8,14 @@ import { loginUrl } from "../api";
 import axios from "axios";
 
 export interface LoginProps {
-    setPatientId: (patientId: string) => void;
+	setPatientId: (patientId: string) => void;
 }
 
 export default function Login(props: LoginProps) {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 	const [emailError, setEmailError] = useState("");
+	const [isPatient, setIsPatient] = useState("0");
 	const history = useHistory();
 
 	const validateForm = () =>
@@ -24,20 +25,23 @@ export default function Login(props: LoginProps) {
 		event,
 	) => {
 		event.preventDefault();
-
-		const resp = axios
-			.post(loginUrl(), { email, password })
-			.then(c => {
-				props.setPatientId(c.data)
-				history.push("/dashboard");
-			})
-			.catch((err) => console.log(err));
+		if (isPatient === "0") {
+			const resp = axios
+				.post(loginUrl(), { email, password })
+				.then((c) => {
+					props.setPatientId(c.data);
+					history.push("/dashboard");
+				})
+				.catch((err) => console.log(err));
+		} else {
+			console.log("Doctor", isPatient);
+			history.push("/dashboard");
+		}
 	};
 
 	return (
 		<React.Fragment>
 			<div id="login">
-				<h3 className="text-center text-white pt-5">Login form</h3>
 				<div className="container">
 					<div
 						id="login-row"
@@ -45,10 +49,38 @@ export default function Login(props: LoginProps) {
 						<div id="login-column" className="col-md-6">
 							<div id="login-box" className="col-md-12">
 								<form id="login-form" className="form" onSubmit={handleSubmit}>
-									<h3 className="text-center text-info">Login</h3>
+									<h3 className="text-center text-info">Sign In</h3>
+									<br></br>
+
+									<div className="text-center">
+										<div className="form-check form-check-inline">
+											<input
+												className="form-check-input"
+												type="radio"
+												checked={isPatient === "0"}
+												name="inlineRadioOptions"
+												onChange={(e) => setIsPatient(e.target.value)}
+												id="inlineRadio1"
+												value="0"></input>
+											<label className="form-check-label">Patient</label>
+										</div>
+										<br></br>
+										<br></br>
+										<div className="form-check form-check-inline">
+											<input
+												className="form-check-input"
+												type="radio"
+												name="inlineRadioOptions"
+												checked={isPatient === "1"}
+												onChange={(e) => setIsPatient(e.target.value)}
+												id="inlineRadio2"
+												value="1"></input>
+											<label className="form-check-label">Doctor</label>
+										</div>
+									</div>
 									<div className="form-group">
-										<label htmlFor="username" className="text-info">
-											Username:
+										<label htmlFor="username" className="text-secondary">
+											Email:
 										</label>
 										<input
 											type="text"
@@ -60,7 +92,7 @@ export default function Login(props: LoginProps) {
 											onChange={(e) => setEmail(e.target.value)}></input>
 									</div>
 									<div className="form-group">
-										<label htmlFor="password" className="text-info">
+										<label htmlFor="password" className="text-secondary">
 											Password:
 										</label>
 										<input
@@ -71,20 +103,23 @@ export default function Login(props: LoginProps) {
 											className="form-control"
 											onChange={(e) => setPassword(e.target.value)}></input>
 									</div>
-									<div className="form-group">
+									<div className="text-center">
 										<input
 											type="submit"
 											name="submit"
 											disabled={!validateForm()}
-											className="btn btn-info btn-md"
-											value="Login"></input>
+											className="btn btn-primary"
+											value="Sign In"></input>
 									</div>
 									<br></br>
 									<br></br>
 									<br></br>
-									<div id="register-link" className="form-group">
+									<br></br>
+
+									<div id="register-link" className="text-center">
+										New User?
 										<a href="/Register" className="text-info">
-											Register here
+											Signup
 										</a>
 									</div>
 								</form>
