83d24f66efd3aaf86857d8be5df21a75389137e4
jlohith <lohithsinghj.5@gmail.com>
2021-04-07 17:30:01 +0530

    LoginCOsmeticchanges

--- smart-health/src/api.ts
+++ smart-health/src/api.ts
@@ -2,90 +2,78 @@ import axios, { AxiosResponse } from "axios";
 import IPatient from "./entities/IPatient";
 
 export function getUrl(): string {
-    return `http://localhost:8080/api/`;
+	return `http://localhost:8080/api/`;
 }
 
 export function loginUrl(): string {
-    return `${getUrl()}/login`;
+	return `${getUrl()}/login`;
+}
+
+export function doctorLoginUrl(): string {
+	return `${getUrl()}/doctorLogin`;
 }
 
 export async function registerUser(user: any): Promise<boolean> {
-    console.log(user);
-    await axios.post(
-      "http://localhost:8080/api/create/patient",
-      {...user}
-    )
-    return true;
+	console.log(user);
+	await axios.post("http://localhost:8080/api/create/patient", { ...user });
+	return true;
 }
-
-
-
 
 export async function getAllergens(patient: string) {
-    const allergens = [
-        "Food Allergy- Egg, Milk, Mustard",
-        "Food Allergy- Egg, Milk, Mustard",
-        "Food Allergy- Egg, Milk, Mustard"
-    ]
-    return Promise.resolve({allergens});
+	const allergens = [
+		"Food Allergy- Egg, Milk, Mustard",
+		"Food Allergy- Egg, Milk, Mustard",
+		"Food Allergy- Egg, Milk, Mustard",
+	];
+	return Promise.resolve({ allergens });
 }
 export async function getPatientName(patientId: string): Promise<string> {
-    const patient = await getPatientDetails(patientId);
-    return patient.patientName;
+	const patient = await getPatientDetails(patientId);
+	return patient.patientName;
 }
 
 export async function getPatientDetails(patientId: String): Promise<IPatient> {
-    const response: AxiosResponse<any> = await axios.get(`${getUrl()}/get/patient/${patientId}`);
-    return response.data;
+	const response: AxiosResponse<any> = await axios.get(
+		`${getUrl()}/get/patient/${patientId}`,
+	);
+	return response.data;
 }
 
 export async function getAllAllergens() {
-    const response = await axios.get(`${getUrl()}get/allergy/all`);
-    return response.data;
+	const response = await axios.get(`${getUrl()}get/allergy/all`);
+	return response.data;
 }
 
-export async function saveAllergenDetails(allergy: any, patientId: string): Promise<boolean> {
-    const patient = await getPatientDetails(patientId);
-    await axios.post(
-        `${getUrl()}create/patient/allergy`,
-        {
-            patient,
-            allergy,
-            symptoms: allergy.symptoms
-        }
-    );
-    return true;
+export async function saveAllergenDetails(
+	allergy: any,
+	patientId: string,
+): Promise<boolean> {
+	const patient = await getPatientDetails(patientId);
+	await axios.post(`${getUrl()}create/patient/allergy`, {
+		patient,
+		allergy,
+		symptoms: allergy.symptoms,
+	});
+	return true;
 }
 
 export async function getMedicines(patient: string) {
-    return Promise.resolve(
-        {
-            medicines: [
-                "Medicine 1",
-                "Medicine 2",
-                "Medicine 3"
-            ]
-        }
-    )
+	return Promise.resolve({
+		medicines: ["Medicine 1", "Medicine 2", "Medicine 3"],
+	});
 }
 
 export async function getAllergen(allergenId: string) {
-    const response = await axios.get(`${getUrl()}/get/allergy/${allergenId}`);
-    return response.data;
+	const response = await axios.get(`${getUrl()}/get/allergy/${allergenId}`);
+	return response.data;
 }
 
-export function getPatientInfo(patientId: string){
-    return Promise.resolve(
-       {
-           patientInfo: {
-               allergens: [
-
-               ],
-               medicines: [
-
-               ],
-               name: "Smart Health Pat - 1"
-           }
-       } 
-    );
+export function getPatientInfo(patientId: string) {
+	return Promise.resolve({
+		patientInfo: {
+			allergens: [],
+			medicines: [],
+			name: "Smart Health Pat - 1",
+		},
+	});
 }
--- smart-health/src/common/page.tsx
+++ smart-health/src/common/page.tsx
@@ -1,18 +1,18 @@
-import * as React from 'react';
-import Header from './header';
+import * as React from "react";
+import Header from "./header";
 
 export interface PageProps {
-    patientId: string;
-    title: string;
+	patientId: string;
+	title: string;
 }
- 
+
 const Page: React.FunctionComponent<PageProps> = (props) => {
-    return ( 
-        <React.Fragment>
-            <Header currentPageTitle={props.title} patientId={props.patientId} />
-            {props.children}
-        </React.Fragment>
-     );
-}
- 
+	return (
+		<React.Fragment>
+			<Header currentPageTitle={props.title} patientId={props.patientId} />
+			{props.children}
+		</React.Fragment>
+	);
+};
+
 export default Page;
