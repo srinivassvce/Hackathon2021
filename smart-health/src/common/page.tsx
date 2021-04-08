a4a83ffbf98b88b3d76ba10df6f7c7e8a30caac1
Kumar <avin.kumar@siemens-healthineers.com>
2021-04-08 11:48:45 +0530

    added menu bar and profile section

--- smart-health/src/common/page.tsx
+++ smart-health/src/common/page.tsx
@@ -1,5 +1,7 @@
 import * as React from "react";
+import {Col, Row} from "react-bootstrap";
 import Header from "./header";
+import MenuBar from "./menuBar";
 
 export interface PageProps {
 	patientId: string;
@@ -7,10 +9,15 @@ export interface PageProps {
 }
 
 const Page: React.FunctionComponent<PageProps> = (props) => {
+
+	const [showMenu, setShowMenu] = React.useState(false);
 	return (
 		<React.Fragment>
-			<Header currentPageTitle={props.title} patientId={props.patientId} />
-			{props.children}
+			<Header currentPageTitle={props.title} patientId={props.patientId} displayMenu={setShowMenu} />
+			<Row>
+				{showMenu ? <Col className="left" xs={2}><MenuBar/></Col> : undefined}
+				<Col> {props.children}</Col>
+			</Row>
 		</React.Fragment>
 	);
 };
