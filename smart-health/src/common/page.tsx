4c7a0845f4fd37c9c7f43270e0c61261e07a8d93
Raghavan <srinivassvce@gmail.com>
2021-04-13 16:19:01 +0530

    Dancing issues fixed

--- smart-health/src/common/page.tsx
+++ smart-health/src/common/page.tsx
@@ -14,10 +14,16 @@ const Page: React.FunctionComponent<PageProps> = (props) => {
 	return (
 		<React.Fragment>
 			<Header currentPageTitle={props.title} patientId={props.patientId} displayMenu={setShowMenu} />
-			<Row>
-				{showMenu ? <Col className="left" xs={2.5}><MenuBar/></Col> : undefined}
-				<Col> {props.children}</Col>
-			</Row>
+			{/*Stop dancing*/}
+			<div className={"container-fluid"}>
+				<div className={"row"} >
+					{showMenu ? <Col className="left" xs={2.5}><MenuBar/></Col> : undefined}
+					<div className={"col"}>
+						{props.children}
+					</div>
+				</div>
+			</div>
+
 		</React.Fragment>
 	);
 };
